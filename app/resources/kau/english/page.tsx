45ae0d7ba9a24741638d736f4827e3373d054552
YAOdat <yaserodat@outlook.com>
2024-02-14 12:52:09 +0000

    added a favicon, fixed an error

--- app/resources/kau/english/page.tsx
+++ app/resources/kau/english/page.tsx
@@ -4,20 +4,26 @@ import { Card, CardBody, CardHeader, Image, Link } from "@nextui-org/react";
 const EnglishResources = () => {
   return (
     <div>
-      <h1>English Resources</h1>
-      <p>This is the English resources page for KAU.</p>
+        <head>
+        <title>اللغة الإنجليزية - جامعة الملك عبد العزيز</title>
+        <meta name="description" content="استكشف شروحات، ملخصات، وتسريبات لمنهاج اللغة الإنجليزية بجميع مستوياته. لمنهاج اللغة الإنجليزية  في جامعة الملك عبد العزيز" />
+        <meta name="keywords" content="اللغة الإنجليزية، جامعة الملك عبد العزيز، اللغة الإنجليزية في جامعة الملك عبد العزيز، انقلش جامعة الملك عبد العزيز، انجليزي جامعة الملك عبدالعزيز، شرح انقلش جامعة" />
+        </head>
+      <h1 className="text-2xl font-bold text-center my-4">اللغة الإنجليزية</h1>
+      <p>مصادر تعليمية لمنهاج اللغة الإنجليزية في جامعة الملك عبد العزيز</p>
+      <br></br>
       <Link href="/resources/kau/english/elis101"> 
       <Card className="py-4 max-w-10">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
         <h4 className="font-bold text-large">ELIS 101 انجليزي علمي 1</h4>
         <small className="text-default-500">2023-2024</small>
-        <p dir='rtl' className="text-tiny font-bold whitespace-pre-line">الكلمات المطلوب حفظها بالاختبار النهائي</p>
+        <p dir='rtl' className="text-tiny font-bold whitespace-pre-line">مصادر تعليمية لمنهاج اللغة الإنجليزية المستوى الأول علمي</p>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
           alt="English WhatsApp Group Blog Post"
           className="object-cover rounded-xl"
-          src="/vocabs.jpg"
+          src="/elis.png"
           width={270}
           height={200}
         />
