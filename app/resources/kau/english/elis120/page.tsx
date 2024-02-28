8deb5ac15d81078d06432c89f06ebae9caf72756
YAOdat <yaserodat@outlook.com>
2024-02-28 09:38:42 +0000

    added a reading practice test

--- app/resources/kau/english/elis120/page.tsx
+++ app/resources/kau/english/elis120/page.tsx
@@ -6,6 +6,8 @@ import { Card, CardBody, CardHeader, Image, Link } from "@nextui-org/react";
 
 export default function ELIS120() {
   return (
+    <> 
+    <div className="my-2"> 
     <Link href="/resources/kau/english/elis120/vocabs"> 
     <Card className="py-4 max-w-10">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
@@ -24,6 +26,26 @@ export default function ELIS120() {
       </CardBody>
     </Card>
     </Link>
+    </div>
+    <Link href="/resources/kau/english/elis120/practice"> 
+    <Card className="py-4 max-w-10">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">Mid Practice Test</h4>
+        <small className="text-default-500">2023-2024</small>
+        <p dir='rtl' className="text-tiny font-bold whitespace-pre-line">اختبار تجريبي يحاكي الاختبار الفعلي النصفي</p>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="انجليزي جامعة الملك عبد العزيز"
+          className="object-cover rounded-xl"
+          src="/vocabs.jpg"
+          width={270}
+          height={200}
+        />
+      </CardBody>
+    </Card>
+    </Link>
+    </>
   );
 }
 
