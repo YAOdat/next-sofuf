b0fa3b2dfe5889ffbba5f6d43317eb06a9412267
YAOdat <yaserodat@outlook.com>
2024-04-09 02:01:05 +0000

    added progress quiz solutions

--- app/resources/kau/english/elis120/page.tsx
+++ app/resources/kau/english/elis120/page.tsx
@@ -44,7 +44,27 @@ export default function ELIS120() {
         />
       </CardBody>
     </Card>
+
     </Link>
+    <Link href="/resources/kau/english/elis120/progress-quiz" className="my-2"> 
+    <Card className="py-4 max-w-10">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">Progress Quizzes</h4>
+        <small className="text-default-500">2023-2024</small>
+        <p dir='rtl' className="text-tiny font-bold whitespace-pre-line">حلول اختبارات التقدم في المستوى</p>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="انجليزي جامعة الملك عبد العزيز"
+          className="object-cover rounded-xl"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcoAWPxvTx-M6z5MhO57TCb99S8UUB498DH2nXEWoVUQwrrnTA"
+          width={270}
+          height={200}
+        />
+      </CardBody>
+    </Card>
+    </Link>
+
     </>
   );
 }
