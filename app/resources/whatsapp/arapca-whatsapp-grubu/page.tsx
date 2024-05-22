1bc996cc3c20756a20126354c838cc522a5577b8
YAOdat <yaserodat@outlook.com>
2024-05-22 12:05:21 +0000

    Added a static page

---
+++ app/resources/whatsapp/arapca-whatsapp-grubu/page.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { Card, CardBody, Spacer, Link, Code, Image } from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
+import type { Metadata } from 'next'
+import CommentSection from "../../../../components/commentSection";
+
+const arabicGroups = [
+  { name: 'Arapça WhatsApp Grubu', link: 'https://chat.whatsapp.com/GC59qnSlRtf9tC4NjqUuYY', language: 'Arabic' },
+  { name: 'English Arabic Group', link: 'https://chat.whatsapp.com/DNAiOzxCkAVCsDA2oNnlJd', language: 'EnglishArabic' }
+
+];
+
+export const metadata: Metadata = {
+  title: 'Arapça WhatsApp Grubu',
+  keywords: ['Arapça WhatsApp grubu', 'Arapça WhatsApp grubu bağlantısı', 'WhatsApp\'tan Arapça öğrenmek', 'Arapça konuşma pratiği WhatsApp grubu', 'Arapça öğrenme grubu WhatsApp', 'Arapça dil becerilerini geliştirme WhatsApp grubu', 'Arapça ile konuşma pratiği', 'Arapça konuşma WhatsApp', 'Arapça sohbet grubu', 'Arapça öğrenme uygulaması'], 
+  description: 'Arapça öğrenin ve konuşun! Yeni kelimeler, dilbilgisi kuralları öğrenin ve ana dili Arapça olan kişilerle iletişim kurun. Sadece Arapça!',
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1',
+}
+export default function WhatsApp() {
+  return (
+    <div className="container mx-auto mt-8">
+      <Card>
+        <CardBody>
+          <article className="max-w-full md:max-w-3xl lg:max-w-4xl mx-auto">
+            <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">Arapça WhatsApp Grubu</h1>
+            <p className="text-xs md:text-sm font-thin mb-4">2024 - 05 - 23 / Author: Odat</p>
+            <div className="mb-4">
+              <p className="md:text-base">
+              WhatsApp ile <span className="font-bold">Arapça</span> Konuşma Becerilerinizi Geliştirin!
+              </p >
+              <Link className="font-bold mt-4" href='https://chat.whatsapp.com/GC59qnSlRtf9tC4NjqUuYY' style={{ color: 'cyan' }} isExternal rel="noopener noreferrer">Tap Here to Join</Link>
+            </div>
+            <div className="max-w-full md:max-w-3xl lg:max-w-4xl mx-auto mb-8">
+
+              <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-4">Neden Bu Gruba Katılmalısınız?</h2>
+              <ul className="list-disc pl-6 mb-4">
+                <li className="mb-2"><span className="font-bold"> Deneyimli Arapça öğrencileriyle etkileşim: </span> Sorularınıza cevap alın ve tecrübelerinizi paylaşın.</li>
+                <li className="mb-2"><span className="font-bold">Günlük konuşma pratiği:</span> Arapça'yı günlük hayatta kullanarak kendinizi geliştirin.</li>
+                <li className="mb-2"><span className="font-bold">Konuşma becerileri üzerinde yoğunlaşma: </span>Dilediğiniz zaman sesli mesaj göndererek konuşma pratiği yapın.</li>
+                <li><span className="font-bold">Destekleyici ortam:</span> Motivasyonunuzu yüksek tutun ve öğrenme sürecinden keyif alın.</li>
+              </ul>
+              <h4 className="font-semibold mb-2">Gruba Katılmadan Önce:</h4>
+              <ol className="list-decimal pl-6 mb-4">
+                <li className="mb-2">Arapça dil bilgisi hakkında temel bilgilere sahip olun.</li>
+                <li className="mb-2">Grup kurallarını okuyun ve uymayı taahhüt edin.(<span className="italic">Settings {">"} Data and Storage Usage {">"} Turn off all downloads</span>).</li>
+                <li className="mb-2">WhatsApp bildirimlerini sessize ayarlayın.</li>
+              </ol>
+              <h3 className="text-lg font-semibold mb-2">Gruba Nasıl Katılırım?</h3>
+              <p className="mb-4">Aşağıdaki bağlantıya tıklayarak gruba hemen katılın! <a target="_blank" href="https://www.whatsapp.com/download" className="text-blue-500">download it here</a>.</p>
+              <h2 className="text-lg md:text-xl lg:text-2xl font-bold mb-4">Gruba Nasıl Katılırım?</h2>
+              <div className="mb-4">              
+                <div className="flex flex-wrap mb-4">
+                  <ul className="list-disc pl-6 mb-4">
+                    {arabicGroups.map((group, index) => (
+                      <li key={index} className="mb-2">
+                        {group.name}:<br></br><Code className="text-blue-500 break-words bg-cyan-900" ><Link href={group.link} className="responsiveLinkSize" target="_blank" rel="noopener noreferrer">{group.link}</Link>
+                        </Code>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="mb-4" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Image
+                alt="Arapça WhatsApp grubu 2024"
+                className="object-cover rounded-xl"
+                src="https://media-sin6-3.cdn.whatsapp.net/v/t61.24694-24/341101225_1109362833789229_8306012962624055979_n.jpg?ccb=11-4&oh=01_Q5AaIEY6Au8Sm8pycCAxSqNp6YmjcClZ-miOs_DU5EF1rkZq&oe=665B0528&_nc_sid=e6ed6c&_nc_cat=100"
+                width={300}
+              /></div>
+
+            <div className="max-w-full md:max-w-2xl mx-auto">
+              <p className="mb-8">
+              Bu Telegram grubu, Arapça öğrenmek isteyen ve WhatsApp kullanmayan kişiler için mükemmel bir fırsattır. Katılın ve dil becerilerinizi geliştirmeye başlayın!
+              </p>
+
+              Arabic Telegram Group:<br /><Code className="text-blue-500 break-words bg-cyan-900 mt-4" ><Link href="https://t.me/ArabicDiscussionGroup" style={{ fontSize: '12px', color: 'cyan' }} target="_blank" rel="noopener noreferrer">https://t.me/ArabicDiscussionGroup</Link>
+              </Code>
+            </div>
+
+          </article>
+        </CardBody>
+      </Card>
+     
+      <Spacer y={1} />
+      
+<CommentSection commenterName={""} comment={""} commentDate={""} postID={"whatsapp-group"} />
+    </div>
+  );
+}
--- app/resources/whatsapp/page.tsx
+++ app/resources/whatsapp/page.tsx
@@ -5,6 +5,8 @@ import { Card, CardBody, CardHeader, Image, Link } from "@nextui-org/react";
 
 export default function WhatsApp() {
   return (
+    
+  <>
     <Link href="/resources/whatsapp/english-whatsapp-group"> 
     <Card className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
@@ -23,6 +25,25 @@ export default function WhatsApp() {
       </CardBody>
     </Card>
     </Link>
+    <Link href="/resources/whatsapp/arapca-whatsapp-grubu">
+    <Card className="py-4">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">Arabic WhatsApp Group</h4>
+        <small className="text-default-500">2023-2024</small>
+        <p className="text-tiny font-bold">Practice and learn Arabic on WhatsApp.</p>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Arabic WhatsApp Group Blog Post"
+          className="object-cover rounded-xl"
+          src="/whatsappgroups.png"
+          width={270}
+          height={200}
+        />
+      </CardBody>
+    </Card>
+    </Link>
+    </>
   );
 }
 
