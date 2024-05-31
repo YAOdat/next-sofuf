8dca8ff60eb96fe4c5cb09eec83983eda1c34461
YAOdat <yaserodat@outlook.com>
2024-05-31 14:56:11 +0000

    seo

--- app/resources/whatsapp/page.tsx
+++ app/resources/whatsapp/page.tsx
@@ -1,13 +1,22 @@
-'use client';
 
 import React from "react";
 import { Card, CardBody, CardHeader, Image, Link } from "@nextui-org/react";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'WhatsApp Group Links',
+  keywords: 'English WhatsApp groups, English WhatsApp group, learn English online, language exchange, English whatsapp group link, practice English on WhatsApp, Best English Language Exchange on WhatsApp, Chat in English on WhatsApp, Free English WhatsApp Group 2024, Arabic WhatsApp group, Turkish whatsapp group, french whatsapp group.',
+  description: 'Find & join active WhatsApp groups for your interests! Free links to connect & chat with people who share your passions.',
+  robots: 'index, follow',
+}
 
 export default function WhatsApp() {
+
+  
   return (
     
   <>
-    <Link href="/resources/whatsapp/english-whatsapp-group"> 
+<h1 className="text-2xl font-bold text-center my-2">WhatsApp Group Links</h1><Link href="/resources/whatsapp/english-whatsapp-group"> 
     <Card className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
         <h4 className="font-bold text-large">English WhatsApp Group</h4>
